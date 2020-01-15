e0a65a5fe0d19621dd37373492ff33894f6aa986
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-15 10:40:23 -0700

    working on 2nd part of assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function getDogImages(num) {
+    for (let i = 0; i<num; i++){
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+        .catch(error => alert('Ruh Roh'));
+    }
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const number = $('.number').val();
+        getDogImages(number);
+    });
+}
+
+function handleDogApp() {
+    watchForm();
+    console.log('hello');
+}
+
+$(handleDogApp());
