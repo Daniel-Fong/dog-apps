41a16fb60451a296aa515aec3f04f6b17f0ad618
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-15 19:02:36 -0700

    still stuck

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 'use strict';
 
 function getDogImages(num) {
-    for (let i = 0; i<num; i++){
-    fetch('https://dog.ceo/api/breeds/image/random')
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayResults(responseJson))
         .catch(error => alert('Ruh Roh'));
-    }
+}
+
+function displayResults(responseJson) {
+    let template = createTemplate(responseJson[0]);
+    console.log(template);
+    // let results = responseJson.forEach(createTemplate());
+    // return $('.results').append(results);
+    console.log(responseJson);
+}
+
+function createTemplate(url) {
+    return `<img class="dogPic" src="${url}"/>`
 }
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
+        $('.results').empty();
         const number = $('.number').val();
         getDogImages(number);
     });
@@ -19,7 +30,6 @@ function watchForm() {
 
 function handleDogApp() {
     watchForm();
-    console.log('hello');
 }
 
 $(handleDogApp());
